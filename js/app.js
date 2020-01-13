cb7282ca3c913279403e81fbde89248ea6c1f372
rlauderdale <rjlauderdale26@gmail.com>
2020-01-12 18:05:10 -0600

    Initial boilerplate and instructions
    +created the initial files for the project.
    +added reminder comments for the project instructions.

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/***
+ Directions For Random Quote Generator
+***/ 
+/* getRandomQuote that returns a random object from the quotes array.
+*/
+/*
+function named printQuote that calls the "getRandomQuote" function. It prints the quote to the page using the "example HTML string format"
+*/
+/****
+EXTRAS
+****/
+/*
+Quotes change automatically after a certain amount of time passes [ use setTimeout()]. Background color changes each time the quote changes.
+*/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
---
+++ js/quotes.js
@@ -0,0 +1,37 @@
+/* array of objects named quotes, array must contain a min of 5 quotes. Each quote object should include a quote and source property. Sould also include a year property.*/
+
+/*
+additional properties such as categorization tags. Additional properties displayed on the page.
+*/
+const quotes = [
+  {
+    quote: '',
+    source:'',
+    citation: '',
+    year:'',
+  },
+  {
+    quote: '',
+    source:'',
+    citation: '',
+    year:'',
+  },
+  {
+    quote: '',
+    source:'',
+    citation: '',
+    year:'',
+  },
+  {
+    quote: '',
+    source:'',
+    citation: '',
+    year:'',
+  },
+  {
+    quote: '',
+    source:'',
+    citation: '',
+    year:'',
+  },
+]
